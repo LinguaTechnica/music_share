2956c2ce8f14d495833ae72c97220cb7225bd6ba
K Hudson <dev@lotech.info>
2021-01-25 02:12:31 -0800

    Init.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Music Share
+
+Music Share is a playlist service. Users can create music lists and share them with others.
+
---
+++ src/test/java/com/lotech/musicshare/MusicshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotech.musicshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
